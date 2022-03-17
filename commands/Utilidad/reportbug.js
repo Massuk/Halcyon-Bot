200479e274f2780a10cc972d4a966911f05b2336
Massuk <miguelrhcp14@gmail.com>
2022-03-17 12:48:40 -0500

    First commit

--- commands/Utilidad/reportbug.js
+++ commands/Utilidad/reportbug.js
@@ -6,19 +6,19 @@ module.exports = {
 	description: 'Envia un reporte al desarrollador',
     usage: [` <reporte>`],
     cooldown: 5,
-	run: async (client, message, args) => 
-  {
+	run: async (client, message, args) =>  {
+    
+    let owner = await client.users.fetch('682588153407995961')
+    
     const reporte = args.join(" ")
     if (!reporte) return message.channel.send("Debes escribir el reporte!")
 
-    const embed = new Discord.MessageEmbed()
-
-    .setTitle(`Se ha emitido un reporte desde **${client.guild}**`)
+    owner.send(new Discord.MessageEmbed()
+    .setTitle(`Se ha emitido un reporte desde **${guild.name}**`)
     .setDescription(`El usuario **${message.author.username}** ha reportado lo siguiente`)
     .setImage('https://instabug.com/blog/wp-content/uploads/2021/01/b_How-to-Write-a-Bug-Report-The-Ideal-Bug-Report--1024x480.png')
     .setTimestamp()
     .setColor("BLACK")
-
-    client.users.cache.get('682588153407995961').send(embed)
+  )
 	},
 };
